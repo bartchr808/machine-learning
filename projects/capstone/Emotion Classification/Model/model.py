6d0b8a28f09759b67f795ca08aab30562e25f268
bartchr808 <bart.chr@gmail.com>
2017-07-08 19:36:08 -0400

    Got model to work, however, without APL units and fractional max pooling

--- projects/capstone/Emotion Classification/Model/model.py
+++ projects/capstone/Emotion Classification/Model/model.py
@@ -1,44 +1,58 @@
 # All necessary imports
 import numpy as np
-from keras.models import sequential
-from keras.layers import Dense, Dropout, Flatten
+import h5py
+from keras.models import Sequential, Model
+from keras.layers import Dense, Dropout, Flatten, InputLayer
 from keras.layers.convolutional import Conv2D, MaxPooling2D, ZeroPadding2D
 from keras.utils import np_utils
 from keras import backend as K
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers.advanced_activations import PReLU
+import tensorflow as tf
 
 seed = 7
-numpy.random.seed(seed)
+np.random.seed(seed)
 
 
-def model(weights = None, drop_rate = 0.0):
+def model(weights = None, drop_rate = .5, p_ratio=[1.0, 1.44, 1.44, 1.0]):
     model = Sequential()
-    model.add(ZeroPadding2D(2,2), input_shape=(1, 48, 48))
-    model.add(Conv2D(320, (3, 3), activation='PReLU'))
-    model.add(ZeroPadding2D(1,1))
-    model.add(tf.nn.fractional_max_pool(Conv2D(320, (3, 3), activation='PReLU')))
+    #model.load_weights('./first_try.h5')
+    model.add(ZeroPadding2D((2,2), input_shape=(48, 48, 3)))
+    model.add(Conv2D(320, (3, 3)))
+    model.add(PReLU())
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(320, (3, 3)))
+    model.add(PReLU())
+    model.add(MaxPooling2D((2,2), strides=(2,2), dim_ordering="th"))
 
-    model.add(ZeroPadding2D(1,1))
-    model.add(Conv2D(640, (3, 3), activation='PReLU'))
-    model.add(ZeroPadding2D(1,1))
-    model.add(Conv2D(640, (3, 3), activation='PReLU'))
-    #model.add(Input(tensor=tf.nn.fractional_max_pool()))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(640, (3, 3)))
+    model.add(PReLU())
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(640, (3, 3)))
+    model.add(PReLU())
+    model.add(MaxPooling2D((2,2), strides=(2,2), dim_ordering="th"))
 
-    model.add(ZeroPadding2D(1,1))
-    model.add(Conv2D(960, 3, 3, activation='PReLU'))
-    model.add(ZeroPadding2D(1,1))
-    model.add(Conv2D(960, 3, 3, activation='PReLU'))
-    #model.add(Input(tensor=tf.nn.fractional_max_pool()))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(960, 3, 3))
+    model.add(PReLU())
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(960, 3, 3))
+    model.add(PReLU())
+    model.add(MaxPooling2D((2,2), strides=(2,2), dim_ordering="th"))
 
-    model.add(ZeroPadding2D(1,1))
-    model.add(Conv2D(1280, 3, 3, activation='PReLU'))
-    model.add(ZeroPadding2D(1,1))
-    model.add(Conv2D(1280, 3, 3, activation='PReLU'))
-    #model.add(Input(tensor=tf.nn.fractional_max_pool()))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(1280, 3, 3))
+    model.add(PReLU())
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(1280, 3, 3))
+    model.add(PReLU())
+    model.add(MaxPooling2D((2,2), strides=(2,2), dim_ordering="th"))
 
     model.add(Flatten())
-    model.add(Dense(1600, activation='PReLU'))
+    model.add(Dense(1600, activation=PReLU()))
     model.add(Dropout(drop_rate))
-    model.add(Dense(1600, activation='PReLU'))
+    model.add(Dense(1600, activation=PReLU()))
     model.add(Dropout(drop_rate))
     model.add(Dense(7, activation='softmax'))
 
@@ -50,9 +64,34 @@ def model(weights = None, drop_rate = 0.0):
     return model
 
 # build the model
-model = larger_model()
+model = model()
+
+batch_size = 16
+
+train_datagen = ImageDataGenerator(
+    rescale=1.,
+    horizontal_flip=False,
+    vertical_flip=False,
+    fill_mode='nearest')
+
+test_datagen = ImageDataGenerator(rescale=1.)
+
 # Fit the model
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200)
-# Final evaluation of the model
-scores = model.evaluate(X_test, y_test)
-print("Large CNN Error: %.2f%%" % (100-scores[1]*100))
+train_generator = train_datagen.flow_from_directory(
+        '../Training',  # this is the target directory
+        target_size=(48, 48),  # all images will be resized to 150x150
+        batch_size=batch_size)  # since we use binary_crossentropy loss, we need binary labels
+
+# this is a similar generator, for validation data
+validation_generator = test_datagen.flow_from_directory(
+        '../PublicTest',
+        target_size=(48, 48),
+        batch_size=batch_size)
+
+model.fit_generator(
+        train_generator,
+        steps_per_epoch=50 // batch_size,
+        epochs=1,
+        validation_data=validation_generator,
+        validation_steps=50 // batch_size)
+model.save_weights('first_try.h5')  # always save your weights after training or during training

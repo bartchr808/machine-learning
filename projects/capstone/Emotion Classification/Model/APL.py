621932cf08352323352738ed7b3e7ecdb5365d36
bartchr808 <bart.chr@gmail.com>
2017-07-09 03:01:48 -0400

    Got fractional max pooling working using InputLayer. APLUnits works successfully, but not sure if the weights are being trained correctly yet.

--- projects/capstone/Emotion Classification/Model/APL.py
+++ projects/capstone/Emotion Classification/Model/APL.py
@@ -1,3 +1,11 @@
+# All necessary imports
+import numpy as np
+from keras.layers import initializers
+from keras.layers import regularizers
+from keras.layers import constraints
+from keras.engine import InputSpec
+from keras.engine.topology import Layer
+
 class APLUnit(Layer):
     """Adaptive Piecewise Linear Units.
     It follows:
@@ -10,9 +18,10 @@ class APLUnit(Layer):
     # Output shape
         Same shape as the input.
     # Arguments
-        alpha_initializer: initializer function for the weights.
-        alpha_regularizer: regularizer for the weights.
-        alpha_constraint: constraint for the weights.
+        S: number of piecewise functions.
+        alpha and b _initializer: initializer function for the weights.
+        alpha and b _regularizer: regularizer for the weights.
+        alpha and b _constraint: constraint for the weights.
         shared_axes: the axes along which to share learnable
             parameters for the activation function.
             For example, if the incoming feature maps
@@ -27,8 +36,12 @@ class APLUnit(Layer):
     """
 
     def __init__(self, alpha_initializer='zeros',
+                 b_initializer='zeros',
+                 S=1,
                  alpha_regularizer=None,
+                 b_regularizer=None,
                  alpha_constraint=None,
+                 b_constraint=None,
                  shared_axes=None,
                  **kwargs):
         super(APLUnit, self).__init__(**kwargs)
@@ -36,12 +49,18 @@ class APLUnit(Layer):
         self.alpha_initializer = initializers.get(alpha_initializer)
         self.alpha_regularizer = regularizers.get(alpha_regularizer)
         self.alpha_constraint = constraints.get(alpha_constraint)
+        self.b_initializer = initializers.get(b_initializer)
+        self.b_regularizer = regularizers.get(b_regularizer)
+        self.b_constraint = constraints.get(b_constraint)
         if shared_axes is None:
             self.shared_axes = None
         elif not isinstance(shared_axes, (list, tuple)):
             self.shared_axes = [shared_axes]
         else:
             self.shared_axes = list(shared_axes)
+        self.S = S
+        self.alpha_arr=[]
+        self.b_arr=[]
 
     def build(self, input_shape):
         param_shape = list(input_shape[1:])
@@ -50,11 +69,17 @@ class APLUnit(Layer):
             for i in self.shared_axes:
                 param_shape[i - 1] = 1
                 self.param_broadcast[i - 1] = True
-        self.alpha = self.add_weight(shape=param_shape,
-                                     name='alpha',
-                                     initializer=self.alpha_initializer,
-                                     regularizer=self.alpha_regularizer,
-                                     constraint=self.alpha_constraint)
+        for i in range(self.S):
+            self.alpha_arr.append(self.add_weight(shape=param_shape,
+                                        name='alpha_' + str(i),
+                                        initializer=self.alpha_initializer,
+                                        regularizer=self.alpha_regularizer,
+                                        constraint=self.alpha_constraint))
+            self.b_arr.append(self.add_weight(shape=param_shape,
+                                        name='b_' + str(i),
+                                        initializer=self.b_initializer,
+                                        regularizer=self.b_regularizer,
+                                        constraint=self.b_constraint))
         # Set input spec
         axes = {}
         if self.shared_axes:
@@ -65,20 +90,22 @@ class APLUnit(Layer):
         self.built = True
 
     def call(self, inputs, mask=None):
-        pos = K.relu(inputs)
-        if K.backend() == 'theano':
-            neg = (K.pattern_broadcast(self.alpha, self.param_broadcast) *
-                   (inputs - K.abs(inputs)) * 0.5)
-        else:
-            neg = -self.alpha * K.relu(-inputs)
-        return pos + neg
+        max_a = tf.maximum(0., inputs)
+        max_b = 0
+        for i in range(self.S):
+            max_b += self.alpha_arr[i] * tf.maximum(0., -inputs + self.b_arr[i])
+
+        return max_a + max_b
 
     def get_config(self):
         config = {
-            'alpha_initializer': initializers.serialize(self.alpha_initializer),
-            'alpha_regularizer': regularizers.serialize(self.alpha_regularizer),
-            'alpha_constraint': constraints.serialize(self.alpha_constraint),
+            'alpha_initializer': initializers.serialize(self.b_initializer),
+            'alpha_regularizer': regularizers.serialize(self.b_regularizer),
+            'alpha_constraint': constraints.serialize(self.b_constraint),
+            'b_initializer': initializers.serialize(self.b_initializer),
+            'b_regularizer': regularizers.serialize(self.b_regularizer),
+            'b_constraint': constraints.serialize(self.b_constraint),
             'shared_axes': self.shared_axes
         }
-        base_config = super(PReLU, self).get_config()
+        base_config = super(APLUnit, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
--- projects/capstone/Emotion Classification/Model/model.py
+++ projects/capstone/Emotion Classification/Model/model.py
@@ -16,71 +16,70 @@ np.random.seed(seed)
 
 def model(weights = None, drop_rate = .5, p_ratio=[1.0, 1.44, 1.44, 1.0]):
     model = Sequential()
-    #model.load_weights('./first_try.h5')
     model.add(ZeroPadding2D((2,2), input_shape=(48, 48, 3)))
-    model.add(Conv2D(320, (3, 3)))
+    model.add(Conv2D(64, (3, 3)))
     model.add(PReLU())
     model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(320, (3, 3)))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(PReLU())
+    model.add(InputLayer(input_tensor=tf.nn.fractional_max_pool(model.layers[3].output, p_ratio)[0]))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(128, (3, 3)))
+    model.add(PReLU())
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(128, (3, 3)))
     model.add(PReLU())
     model.add(MaxPooling2D((2,2), strides=(2,2), dim_ordering="th"))
 
     model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(640, (3, 3)))
+    model.add(Conv2D(256, 3, 3))
     model.add(PReLU())
     model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(640, (3, 3)))
-    model.add(PReLU())
-    model.add(MaxPooling2D((2,2), strides=(2,2), dim_ordering="th"))
-
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(960, 3, 3))
-    model.add(PReLU())
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(960, 3, 3))
+    model.add(Conv2D(256, 3, 3))
     model.add(PReLU())
     model.add(MaxPooling2D((2,2), strides=(2,2), dim_ordering="th"))
 
     model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(1280, 3, 3))
+    model.add(Conv2D(512, 3, 3))
     model.add(PReLU())
     model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(1280, 3, 3))
+    model.add(Conv2D(512, 3, 3))
     model.add(PReLU())
     model.add(MaxPooling2D((2,2), strides=(2,2), dim_ordering="th"))
 
     model.add(Flatten())
-    model.add(Dense(1600, activation=PReLU()))
+    model.add(Dense(900, activation=PReLU()))
     model.add(Dropout(drop_rate))
-    model.add(Dense(1600, activation=PReLU()))
+    model.add(Dense(900, activation=PReLU()))
     model.add(Dropout(drop_rate))
     model.add(Dense(7, activation='softmax'))
 
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
     if weights:
-        model.load_weights(weights_path)
+        model.load_weights(weights)
 
     return model
 
 # build the model
 model = model()
 
-batch_size = 16
+batch_size = 5
 
 train_datagen = ImageDataGenerator(
-    rescale=1.,
-    horizontal_flip=False,
-    vertical_flip=False,
+    rescale=1./255,
+    horizontal_flip=True,
+    vertical_flip=True,
     fill_mode='nearest')
 
-test_datagen = ImageDataGenerator(rescale=1.)
+test_datagen = ImageDataGenerator(rescale=1./255)
 
 # Fit the model
 train_generator = train_datagen.flow_from_directory(
         '../Training',  # this is the target directory
-        target_size=(48, 48),  # all images will be resized to 150x150
-        batch_size=batch_size)  # since we use binary_crossentropy loss, we need binary labels
+        target_size=(48, 48),  # all images will be resized to 48x48
+        batch_size=batch_size)
 
 # this is a similar generator, for validation data
 validation_generator = test_datagen.flow_from_directory(
@@ -90,8 +89,8 @@ validation_generator = test_datagen.flow_from_directory(
 
 model.fit_generator(
         train_generator,
-        steps_per_epoch=50 // batch_size,
+        steps_per_epoch=20,  #28709 // batch_size,
         epochs=1,
         validation_data=validation_generator,
-        validation_steps=50 // batch_size)
-model.save_weights('first_try.h5')  # always save your weights after training or during training
+        validation_steps=20) #3589 // batch_size)
+#model.save_weights('first_try.h5')  # always save your weights after training or during training
